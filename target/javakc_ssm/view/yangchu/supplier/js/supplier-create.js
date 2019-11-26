972a431da57ccf14b3cd17a54ea2096f84ad3672
rnejianjun2 <renjianjun2@qq.com>
2019-11-26 19:10:16 +0800

    任建军--供应商上传3

--- target/javakc_ssm/view/yangchu/supplier/js/supplier-create.js
+++ target/javakc_ssm/view/yangchu/supplier/js/supplier-create.js
@@ -11,23 +11,23 @@ $(function () {
         });
     });
 
-    $('form:eq(0)').bootstrapValidator({
-        excluded: [":disabled"],
-        trigger: "change",
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            //校验供应商类型
-            supplierType: {
-                validators: {
-                    notEmpty: {
-                        message: '请选择供应商类型！！！'
-                    },
-                }
-            },
+    // $('form:eq(0)').bootstrapValidator({
+    //     excluded: [':disabled'],
+    //     trigger: "change",
+    //     feedbackIcons: {
+    //         valid: 'glyphicon glyphicon-ok',
+    //         invalid: 'glyphicon glyphicon-remove',
+    //         validating: 'glyphicon glyphicon-refresh'
+    //     },
+    //     fields: {
+    //         //校验供应商类型
+    //         supplierType: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '请选择供应商类型！'
+    //                 },
+    //             }
+    //         },
             //校验组织名称
             // orgname: {
             //     validators: {
@@ -38,67 +38,93 @@ $(function () {
             //     }
             // },
             //校验供应商名称
-            supplierName: {
-                validators: {
-                    notEmpty: {
-                        message: '供应商名称不能为空！！！'
-                    },
-                    stringLength: {
-                        min: 2,
-                        max: 30,
-                        message: '供应商名称长度不能低于3不能高于10'
-                    }
-                }
-            },
-            //校验供应商分类
-            supplierLevel: {
-                validators: {
-                    notEmpty: {
-                        message: '请选择供应商分类！！！'
-                    },
-                }
-            },
-            //供应商简称校验
-            supplierSim:{
-                validators: {
-                    notEmpty: {
-                        message: '供应商名称不能为空！！！'
-                    },
-                    stringLength: {
-                        min: 2,
-                        max: 30,
-                        message: '供应商名称长度不能低于3不能高于10'
-                    }
-                }
-          },
-            //供应商法人校验
-            supplierFaren: {
-                validators: {
-                    notEmpty: {
-                        message: '供应商名称不能为空！！！'
-                    },
-                    stringLength: {
-                        min: 6,
-                        max: 15,
-                        message: '供应商名称长度不能低于2不能高于5'
-                    }
+    //         supplierName: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '供应商名称不能为空！！！'
+    //                 },
+    //                 stringLength: {
+    //                     min: 2,
+    //                     max: 30,
+    //                     message: '供应商名称长度不能低于3不能高于10'
+    //                 }
+    //             }
+    //         },
+    //         //校验供应商分类
+    //         supplierLevel: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '请选择供应商分类！！！'
+    //                 },
+    //             }
+    //         },
+    //         //供应商简称校验
+    //         supplierSim:{
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '供应商名称不能为空！！！'
+    //                 },
+    //                 stringLength: {
+    //                     min: 2,
+    //                     max: 30,
+    //                     message: '供应商名称长度不能低于3不能高于10'
+    //                 }
+    //             }
+    //       },
+    //         //供应商法人校验
+    //         supplierFaren: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '供应商名称不能为空！！！'
+    //                 },
+    //                 stringLength: {
+    //                     min: 6,
+    //                     max: 15,
+    //                     message: '供应商名称长度不能低于2不能高于5'
+    //                 }
+    //
+    //             }
+    //         },
+    //     // 供应商法人电话
+    //         supplierFarenph: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '供应商名称不能为空！'
+    //                 },
+    //                 regexp: {
+    //                     regexp:/^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/,
+    //                     message:'请输入标准的大陆手机号！'
+    //                 }
+    //             }
+    //         },
+    //         //供应商地址校验
+    //         supplierAddr: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '供应商地址不能为空！'
+    //                 },
+    //                 stringLength: {
+    //                     min: 2,
+    //                     max: 30,
+    //                     message: '供应商名称长度不能低于2不能高于10'
+    //                 }
+    //         }
+    //         },
+    //         //社会统一信任代码校验
+    //         supplierTrustnum: {
+    //             validators: {
+    //                 notEmpty: {
+    //                     message: '社会统一信任代码不能为空！'
+    //                 },
+    //                 regexp:{
+    //
+    //                 }
+    //             }
+    //         }
+    //
+    //
+    //     }
+    // });
 
-                }
-            },
-        // 供应商法人电话
-            supplierAddr: {
-                validators: {
-                    notEmpty: {
-                        message: '供应商名称不能为空！！！'
-                    },
-                    regexp: {
-                        regexp:
-                    }
-                }
-            },
-
-
-        }
-    });
 
 });
